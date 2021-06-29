bfa44892ecc48dae5204d21cc3e298180c72af77
정현우 <wijeong@gmail.com>
2021-06-29 11:55:29 +0900

    [set]/ #1/ server setting

---
+++ controllers/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+    login: require('./user/login'),
+    withdraw: require('./user/withdraw'),
+    logout: require('./user/logout'),
+    profile: require('./user/profile/userinfo'),
+    category: require('./user/profile/category'),
+    username: require('./user/profile/username'),
+    password: require('./user/profile/password'),
+    signup: require('./user/signup'),
+    studylog: require('./studylog/todo'),
+    record: require('./studylog/record'),
+    newTodo: require('./todo/new'),
+    checkTodo: require('./todo/check'),
+    deleteTodo: require('./todo/delete'),
+    roomList: require('./room/list'),
+}
--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const cookieParser = require('cookie-parser');
+const { urlencoded } = require('express');
+const Router = require('./routes/index')
+
 const app = express();
-const PORT = 80;
+const PORT = 3000;
+
+app.use(express.json())
+app.use(urlencoded({ extended: false }))
+app.use('/', Router)
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"]
+}))
+app.use(cookieParser());
+
+// app.use('/users', usersRouter);
 
 app.get('/', (req, res) => {
     res.send('hello world!');
 })
 
 app.listen(PORT);
+
+// moudule.exports = app;
