7e335105da208a424247c6623353515086c29800
정현우 <wijeong@gmail.com>
2021-07-02 00:29:51 +0900

    [fix] / fixed oauth login error

--- controllers/oauth/oauthgoogle.js
+++ controllers/oauth/oauthgoogle.js
@@ -5,47 +5,61 @@ const { user } = require('../../models');
 const { generateAccessToken, generateRefreshToken, sendAccessToken,sendRefreshToken } =require('../../middlewares/token')
 
 module.exports = async (req, res) => {
-    const { tokenId }  = req.body
+    const { tokenId, subId }  = req.body
+    
     const ticket = await client.verifyIdToken({
         idToken: tokenId,
         audience: process.env.CLIENT_ID
     });
-    const { name, email, picture } = ticket.getPayload();    
-    console.log('33333', picture)
+    
+    const { name, email, sub } = ticket.getPayload();  
+
+    console.log('33333', ticket.getPayload())
     const userInfo = await user.findOne({
         where: { email: email },
         raw: true
     })
 
-    if(!userInfo){
-        const users = await user.create({
-            email: email,
-            username: name,
-        })
 
-        delete users.dataValues.password;
-        const accessToken = generateAccessToken(users.dataValues);
-        const refreshToken = generateRefreshToken(users.dataValues);
+    if(!subId){
+        if(userInfo){
+            res.status(409).send({
+                message: 'user already exist'
+            })
+        }else{
 
-        sendRefreshToken(res, refreshToken);
+            const users = await user.create({
+                email: email,
+                username: name,
+                subId: sub
+            })
+    
+            delete users.dataValues.password;
+            const accessToken = generateAccessToken(users.dataValues);
+            const refreshToken = generateRefreshToken(users.dataValues);
+    
+            sendRefreshToken(res, refreshToken);
+    
+            res.status(201).send({
+                userId: users.dataValues.id,
+                username: users.dataValues.username, 
+                accessToken
+            })
+        }
+    }else{
+        if(userInfo && subId === userInfo.subId){
+            delete userInfo.password;
+            const accessToken = generateAccessToken(userInfo)
+            const refreshToken = generateRefreshToken(userInfo)
+            
+            sendRefreshToken(res, refreshToken)
+            sendAccessToken(res, accessToken, userInfo.username, userInfo.id)
 
-        res.status(201).send({
-            userId: users.dataValues.id,
-            username: users.dataValues.username, 
-            accessToken
-        })
-    }else if(userInfo){
-        delete userInfo.password;
-        const accessToken = generateAccessToken(userInfo)
-        const refreshToken = generateRefreshToken(userInfo)
-        
-        sendRefreshToken(res, refreshToken)
-        sendAccessToken(res, accessToken, userInfo.username, userInfo.id)
+        }else{
+            res.status(404).send('go to sign up')
+        }
     }
     
-
-    res.status(201)
-    res.json(user)
 }
 
     
