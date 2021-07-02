78efacfe1a848609faa9a3c72c90242c6dad5393
정현우 <wijeong@gmail.com>
2021-07-02 14:07:56 +0900

    [feat] /#34 /complete change password

--- controllers/user/profile/password.js
+++ controllers/user/profile/password.js
@@ -1,3 +1,36 @@
-module.exports = async (req, res) => {
+const { user } = require('../../../models');
+const { verifyAccessToken } = require('../../../middlewares/token');
 
+module.exports = async (req, res) => {
+    const userToken = await verifyAccessToken(req);
+   
+    if(userToken === null){
+        res.status(403).send({
+            message: "access token expired"
+        })
+    }else{
+        const users = await user.findOne({
+            where: {
+                email: userToken.email,
+                password: req.body.currentpassword
+            },
+            raw: true
+        })
+        if(!users){
+            res.status(401).send({
+                message: "wrong password"
+            })
+        }else{
+            await user
+                .update(
+                    {
+                        password: req.body.newpassword
+                    },
+                    { where: { email: user.email } }
+                )
+                res.status(205).send({
+                    message: "ok"
+                })
+        }
+    }
 }
