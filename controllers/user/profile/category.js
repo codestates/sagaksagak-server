11525e3a94b0f8b7702700a4b298f511d0ded61a
정현우 <wijeong@gmail.com>
2021-07-06 16:07:00 +0900

    [complete] / #32 / complete change category logic

--- controllers/user/profile/category.js
+++ controllers/user/profile/category.js
@@ -1,3 +1,21 @@
-module.exports = async (req, res) => {
+const { user } = require('../../../models')
+const { verifyAccessToken } = require('../../../middlewares/token')
 
+module.exports = async (req, res) => {
+    const userToken = verifyAccessToken(req);
+
+    if (!userToken) {
+        res.status(403).send({
+            message: 'ok'
+        })
+    } else {
+        const userId = req.params.id;
+        await user
+            .update({
+                category: req.body.newcategory
+            }, { where: {id: userId} })
+            res.status(201).send({
+                message: 'ok'
+            })
+    }
 }
