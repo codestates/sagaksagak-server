ef396eff91d1f11e732e3a64197bb15808c421f5
Hwangsooneun <catalyst8849@gmail.com>
2021-07-17 20:40:15 +0900

    urgency

--- controllers/user/logout.js
+++ controllers/user/logout.js
@@ -1,11 +1,15 @@
 module.exports = async (req, res) => {
     const { authorization } = req.headers
 
-    if(!authorization) {
+    if (!authorization) {
         res.status(403).send()
-    }else{
+    } else{
         delete authorization;
-        res.cookie("refreshToken", '');
+        res.cookie("refreshToken", '', {
+            domain: 'sagaksagak.site',
+            maxAge: 0,
+            secure: true
+        });
         res.status(205).send({
             message: "ok"
         })
